0e0cfb1a75e9c82b96ce57b74eebeb95dc505728
Vladislav <tolkachew.wlad@yandex.ru>
2022-12-12 19:14:04 +0300

    test

--- src/Test1.java
+++ src/Test1.java
@@ -1,2 +1,47 @@
-package PACKAGE_NAME;public class Test1 {
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashSet;
+
+public class Test1 {
+    public static void main(String[] args) {
+        HashSet<Integer> hashSet = new HashSet<Integer>();
+        ArrayList<Integer> myList = new ArrayList<Integer>(hashSet);
+        Collections.sort(myList);
+        System.out.println(myList);
+        HashSet<Student> empHash = new HashSet<Student>();
+        empHash.add(new Student("Дмитрий", 6, "Математика"));
+        empHash.add(new Student("Михаил", 5, "Математика"));
+        empHash.add(new Student("Владислав", 4, "Математика"));
+        empHash.add(new Student("Александр", 9, "Математика"));
+        empHash.add(new Student("Александр2", 8, "Математика"));
+        ArrayList<Student> empList = new ArrayList<Student>(empHash);
+        Collections.sort(empList);
+        System.out.println(empList);
+    }
+}
+
+class Student implements Comparable<Student> {
+    String name;
+    int mark;
+    String subject;
+
+    public Student(String name, int mark, String subject) {
+        super();
+        this.name = name;
+        this.mark = mark;
+        this.subject = subject;
+    }
+    @Override
+    public int compareTo(Student o) {
+        if (this.mark < o.mark) {
+            return -1;
+        } else if (this.mark > o.mark) {
+            return 1;
+        }
+        return 0;
+    }
+    @Override
+    public String toString() {
+        return "Student [name=" + name + ", mark=" + mark + ", subject=" + subject + "]";
+    }
 }
--- src/WriteBin.java
+++ src/WriteBin.java
@@ -1,2 +1,21 @@
-package PACKAGE_NAME;public class WriteBin {
+import java.io.DataOutputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+public class WriteBin {
+    public static void main(String[] args) {
+        String text = "Hello world";
+        byte[] buffer = text.getBytes();
+        try (FileOutputStream fos = new FileOutputStream("src/data/data.bin", false);
+             DataOutputStream dout = new DataOutputStream(fos);) {
+            dout.writeUTF(text);
+            {
+            }
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
